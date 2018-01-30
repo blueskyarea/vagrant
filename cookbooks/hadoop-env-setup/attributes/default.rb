80264df88c0f42a6b6e5504083af1a2cf71996d9
mh <blueskyareahm@gmail.com>
2018-01-31 01:50:52 +0900

    create hadoop dir.

--- cookbooks/hadoop-env-setup/attributes/default.rb
+++ cookbooks/hadoop-env-setup/attributes/default.rb
@@ -1,1 +1,2 @@
-['hdp']['repo']="http://public-repo-1.hortonworks.com/HDP/centos6/2.x/updates/2.6.2.0/hdp.repo"
+default[:hdp][:repo]="http://public-repo-1.hortonworks.com/HDP/centos6/2.x/updates/2.6.2.0/hdp.repo"
+default[:tmp][:work]="/hadoop-tmp-work"
--- cookbooks/hadoop-env-setup/recipes/default.rb
+++ cookbooks/hadoop-env-setup/recipes/default.rb
@@ -30,5 +30,27 @@ directory '/hadoop-tmp-work' do
   action :create
 end
 
+# Copying modules for hadoop
+%w(
+  hadoopDir.conf
+  usersAndGroups.conf
+  createHadoopDir.sh
+).each do |module_name|
+  cookbook_file "/hadoop-tmp-work/#{module_name}" do
+    source "#{module_name}"
+    owner 'root'
+    group 'root'
+    mode '0755'
+  end
+end
+
+# Creating hadoop directory
+bash 'create_hadoop_directory' do
+  user "root"
+  code <<-EOL
+    "/hadoop-tmp-work"/createHadoopDir.sh > "/hadoop-tmp-work"/createHadoopDir.log
+  EOL
+end
+
 log '>>>>>>>>  hadoop env setup done <<<<<<<<<<'
 
