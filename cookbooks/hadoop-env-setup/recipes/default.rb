d4362adfa0055e09697d0c1c3ba5772e2b663d0b
mh <blueskyareahm@gmail.com>
2018-02-02 01:18:38 +0900

    deploy configuration for hadoop.

--- cookbooks/hadoop-env-setup/recipes/default.rb
+++ cookbooks/hadoop-env-setup/recipes/default.rb
@@ -52,5 +52,66 @@ bash 'create_hadoop_directory' do
   EOL
 end
 
+# Creating hadoop conf directory
+directory '/hadoop_conf' do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
+end
+
+# Copying hadoop conf files
+%w(
+  core-site.xml
+  hadoop-env.sh
+  hdfs-site.xml
+  mapred-env.sh
+  mapred-site.xml
+  slaves
+  yarn-env.sh
+  yarn-site.xml 
+).each do |conf_file_name|
+  cookbook_file "/hadoop_conf/#{conf_file_name}" do
+    source "conf/#{conf_file_name}"
+    owner 'root'
+    group 'root'
+    mode '0755'
+  end
+end
+
+bash 'deploy_hadoop_conf' do
+  user "root"
+  code <<-EOL
+    if [ -d /etc/hadoop/conf.bak ]; then
+      rm -rf /etc/hadoop/conf.bak
+    fi
+
+    if [ -d /etc/hadoop/conf ]; then 
+      mv /etc/hadoop/conf /etc/hadoop/conf.bak
+    fi
+
+    mkdir -p /etc/hadoop
+    cp -r /hadoop_conf /etc/hadoop/conf
+  EOL
+end
+
+# Deploying .bash_profile for each user
+bash 'deploy_common_bash_profile' do
+  user "root"
+  code <<-EOL
+    HDFS_HOME=$(eval echo ~hdfs)
+    cp /hadoop-tmp-work/hadoop_bash_profile ${HDFS_HOME}/.bash_profile
+
+    YARN_HOME=$(eval echo ~yarn)
+    cp /hadoop-tmp-work/hadoop_bash_profile ${YARN_HOME}/.bash_profile
+
+    MAPRED_HOME=$(eval echo ~mapred)
+    cp /hadoop-tmp-work/hadoop_bash_profile ${MAPRED_HOME}/.bash_profile
+
+    ZOOKEEPER_HOME=$(eval echo ~zookeeper)
+    cp /hadoop-tmp-work/hadoop_bash_profile ${ZOOKEEPER_HOME}/.bash_profile
+  EOL
+end
+
 log '>>>>>>>>  hadoop env setup done <<<<<<<<<<'
 
